b1dd4198cb12ddb44079fe2aa833363002f13922
ProKafelek12e <pro.kafelek@gmail.com>
2024-09-23 19:15:09 +0200

    19/20 zadania

---
+++ src/app/components/ListWithObjects.js
@@ -0,0 +1,6 @@
+function ListWithObjects({ items }) {
+    return (
+      <ul>      {items.map((item, index) => (        <li key={index}>          {item.name}: {item.value}        </li>
+        ))}
+      </ul>  );
+  }export default ListWithObjects;
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 import ItemList from "./components/ItemList";
 import TextBlock from "./components/Textblock";
 import ImageWithText from "./components/ImageWithText";
+import SimpleForm from "./components/SimpleForm";
+import ConditionalDisplay from "./components/ConditionalDisplay";
+import ListWithObjects from "./components/ListWithObjects";
 import Link from "next/link";
 
 export default function Home() {
@@ -13,6 +16,9 @@ export default function Home() {
         <ImageWithText imageUrl={'http://www.zs1mm.home.pl/strona/wp-content/uploads/2022/03/cropped-korona.png'} text={'Zespół szkól nr. 1'}/>
         <TextBlock heading={"Karol Kędziora"} content={"Technik Programista"}/>
         <ItemList items={['test','test2']}/>
+        <SimpleForm/>
+        <ConditionalDisplay isVisible={true}/>
+        <ListWithObjects items={[{name:'nazwa',value:'wartośc'},{name:'nazwa2',value:'wartośc2'}]}/>
       </main>
     </div>
   );
